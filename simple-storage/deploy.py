82529fbc640cef7e32b03c94dafdaf062fa4a459
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-05-12 01:58:31 +0600

    contract create, connection to ganache

--- simple-storage/deploy.py
+++ simple-storage/deploy.py
@@ -1,5 +1,6 @@
 from solcx import compile_standard, install_solc
 import json
+from web3 import Web3
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -30,3 +31,12 @@ bytecode = compliled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm
 
 # get abi
 abi = compliled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# connection to ganache
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 5777
+my_address = "0xf816F7a35d829e059Caf2693b3025fa68cf3f272"
+private_key = "0x68c663351b9b1c80626d9067b9f032afd9b0639df7a239537e050643eeb6da5f"
+
+# Create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)

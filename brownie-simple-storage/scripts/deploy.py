0b3bf629b9c9c89737bf1ea548d4ebbb0e283b3d
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-05-14 01:52:32 +0600

    deployed and updated contract

--- brownie-simple-storage/scripts/deploy.py
+++ brownie-simple-storage/scripts/deploy.py
@@ -1,16 +1,29 @@
-from brownie import accounts, config
+from brownie import accounts, config, SimpleStorage
 
 
 def deploy_simple_storage():
     # Local ganache brownie account
     account = accounts[0]
 
-    # Testnet/Mainnet account
+    # Testnet/Mainnet account with CLI
     # account = accounts.load("rinkeby-testnet-account2")
 
     # Account from ENV
     # account = accounts.add(config["wallets"]["from_key"])
     # print(account)
+
+    # Deploy Contract to Chain
+    simple_storage = SimpleStorage.deploy({"from": account})
+    print(simple_storage)
+
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
 
 
 def main():

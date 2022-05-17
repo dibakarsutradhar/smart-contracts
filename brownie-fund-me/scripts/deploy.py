7706f20a35e2af94b0a5a67093accca2fa037f43
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-05-17 23:39:36 +0600

    refactored deploy script

--- brownie-fund-me/scripts/deploy.py
+++ brownie-fund-me/scripts/deploy.py
@@ -1,5 +1,5 @@
 from brownie import FundMe, network, config, MockV3Aggregator
-from scripts.utils import get_account
+from scripts.utils import get_account, deploy_mocks
 
 
 def deploy_fund_me():
@@ -13,13 +13,8 @@ def deploy_fund_me():
             "eth_usd_price_feed"
         ]
     else:
-        print(f"The active network is {network.show_active()}")
-        print(f"Deploying Mocks...")
-        mock_aggregator = MockV3Aggregator.deploy(
-            18, 2000000000000000000, {"from": account}
-        )
-        price_feed_address = mock_aggregator.address
-        print("Mocks Deployed")
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
 
     fund_me = FundMe.deploy(
         price_feed_address,
--- brownie-fund-me/scripts/utils.py
+++ brownie-fund-me/scripts/utils.py
@@ -1,4 +1,8 @@
-from brownie import network, config, accounts
+from brownie import network, config, accounts, MockV3Aggregator
+from web3 import Web3
+
+DECIMALS = 18
+STARTING_PRICE = 2000
 
 
 def get_account():
@@ -6,3 +10,13 @@ def get_account():
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print(f"Deploying Mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
+        )
+    print("Mocks Deployed")

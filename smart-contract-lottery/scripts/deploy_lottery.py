af84e7bb7f32f3b220bb1a5ffa93b729a5cf68c7
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-05-22 23:24:19 +0600

    VirtualMachineError revert at end_lottery()

--- smart-contract-lottery/scripts/deploy_lottery.py
+++ smart-contract-lottery/scripts/deploy_lottery.py
@@ -1,4 +1,5 @@
-from scripts.utils import get_account, get_contract
+import time
+from scripts.utils import get_account, get_contract, fund_with_link
 from brownie import Lottery, config, network
 
 
@@ -14,7 +15,41 @@ def deploy_lottery():
         publish_source=config["networks"][network.show_active()].get("verify", False),
     )
     print("deployed lottery!")
+    return lottery
+
+
+def start_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    starting_tx = lottery.startLottery({"from": account})
+    starting_tx.wait(1)
+    print("The Lottery is Started!")
+
+
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 100000000
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("You have entered the Lottery!")
+
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    # fund the contract
+    # then end the lottery
+    tx = fund_with_link(lottery.address)
+    tx.wait(1)
+    ending_tx = lottery.endLottery({"from": account})
+    ending_tx.wait(1)
+    time.sleep(180)
+    print(f"{lottery.recentWinner()} is the new winner!")
 
 
 def main():
     deploy_lottery()
+    start_lottery()
+    enter_lottery()
+    end_lottery()
--- smart-contract-lottery/scripts/utils.py
+++ smart-contract-lottery/scripts/utils.py
@@ -3,6 +3,7 @@ from brownie import (
     network,
     config,
     accounts,
+    interface,
     MockV3Aggregator,
     VRFCoordinatorMock,
     LinkToken,
@@ -79,3 +80,16 @@ def deploy_mocks(decimals=DECIMALS, initial_value=STARTING_PRICE):
     # if len(MockV3Aggregator) <= 0:
     #     MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
     print("Mocks Deployed")
+
+
+def fund_with_link(
+    contract_address, account=None, link_token=None, amount=1000000000000000
+):
+    account = account if account else get_account()
+    link_token = link_token if link_token else get_contract("link_token")
+    tx = link_token.transfer(contract_address, amount, {"from": account})
+    # link_token_contract = interface.LinkTokenInterface(link_token.address)
+    # tx = link_token_contract.transfer(contract_address, amount, {"from": account})
+    tx.wait(1)
+    print("Fund Contract!")
+    return tx

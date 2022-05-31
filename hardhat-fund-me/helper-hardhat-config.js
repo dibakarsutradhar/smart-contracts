1570440fdf2e0256d1d232b3a6aaf324e816fc0f
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-05-31 16:07:05 +0600

    verfiy utils

--- hardhat-fund-me/helper-hardhat-config.js
+++ hardhat-fund-me/helper-hardhat-config.js
@@ -3,13 +3,17 @@ const networkConfig = {
     name: 'rinkeby',
     ethUsdPriceFeed: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
   },
+  5: {
+    name: 'goerli',
+    ethUsdPriceFeed: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
+  },
   31337: {
     name: 'localhost',
   },
 };
 
-const developmentChain = ['hardhat', 'localhost'];
+const developmentChains = ['hardhat', 'localhost'];
 const DECIMALS = '8';
 const INITIAL_ANSWER = '20000000000';
 
-module.exports = { networkConfig, developmentChain, DECIMALS, INITIAL_ANSWER };
+module.exports = { networkConfig, developmentChains, DECIMALS, INITIAL_ANSWER };

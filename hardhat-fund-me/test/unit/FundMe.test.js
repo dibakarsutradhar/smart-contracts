d68593b1b73921eeda0cfe29ca5e1361a9b06cee
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-06-02 01:25:44 +0600

    unit test complete

--- hardhat-fund-me/test/unit/FundMe.test.js
+++ hardhat-fund-me/test/unit/FundMe.test.js
@@ -76,5 +76,60 @@ describe('FundMe', async () => {
         endingDeployerBalance.add(gasCost).toString()
       );
     });
+
+    it('allows us to withdraw with multiple funders', async () => {
+      // Arrange
+      const accounts = await ethers.getSigners();
+
+      for (let i = 1; i < 6; i++) {
+        const fundMeConnectedContract = await fundMe.connect(accounts[i]);
+        await fundMeConnectedContract.fund({ value: sendValue });
+      }
+
+      const startingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      );
+      const startingDeployerBalance = await fundMe.provider.getBalance(
+        deployer
+      );
+
+      // Act
+      const transactionResponse = await fundMe.withdraw();
+      const transactionReceipt = await transactionResponse.wait(1);
+      const { gasUsed, effectiveGasPrice } = transactionReceipt;
+      const gasCost = gasUsed.mul(effectiveGasPrice);
+
+      const endingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      );
+      const endingDeployerBalance = await fundMe.provider.getBalance(deployer);
+
+      // Assert
+      assert.equal(endingFundMeBalance, 0);
+      assert.equal(
+        startingFundMeBalance.add(startingDeployerBalance),
+        endingDeployerBalance.add(gasCost).toString()
+      );
+
+      // Make sure that funders are resetted properly
+      await expect(fundMe.funders(0)).to.be.reverted;
+
+      for (i = 1; i < 6; i++) {
+        assert.equal(
+          await fundMe.addressToAmountFunded(accounts[i].address),
+          0
+        );
+      }
+    });
+
+    it('only allows the owner to withdraw', async () => {
+      const accounts = await ethers.getSigners();
+      const attacker = accounts[1];
+      const attackerConnectedContract = await fundMe.connect(attacker);
+
+      await expect(attackerConnectedContract.withdraw()).to.be.revertedWith(
+        'FundMe__NotOwner'
+      );
+    });
   });
 });

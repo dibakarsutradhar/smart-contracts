f7a1843ea93b01c349602eb430a755bfe151fb83
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-06-02 23:07:12 +0600

    unit and staging test

---
+++ hardhat-fund-me/test/staging/FundMe.staging.test.js
@@ -0,0 +1,24 @@
+const { assert } = require('chai');
+const { ethers, getNamedAccounts, network } = require('hardhat');
+const { developmentChains } = require('../../helper-hardhat-config');
+
+developmentChains.includes(network.name)
+  ? describe.skip
+  : describe('FundMe', async () => {
+      let fundMe;
+      let deployer;
+      const sendValue = ethers.utils.parseEther('1');
+
+      beforeEach(async () => {
+        deployer = (await getNamedAccounts()).deployer;
+        fundMe = await ethers.getContract('FundMe', deployer);
+      });
+
+      it('allows people to fund and withdraw', async () => {
+        await fundMe.fund({ value: sendValue });
+        await fundMe.withdraw();
+        const endingBalance = await fundMe.provider.getBalance(fundMe.address);
+
+        assert.equal(endingBalance.toString(), '0');
+      });
+    });

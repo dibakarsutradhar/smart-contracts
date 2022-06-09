2d23e371142e9a0b61b15770761de539aa64fea8
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-06-09 23:44:55 +0600

    network config

---
+++ hardhat-scc-lottery/backend/helper-hardhat-config.js
@@ -0,0 +1,13 @@
+const networkConfig = {
+  // localhost: {},
+  // hardhat: {},
+  4: {
+    name: 'rinkeby',
+    // subscriptionId: "588",
+    // gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+    // keepersUpdateInterval: "30",
+    // raffleEntranceFee: "100000000000000000", // 0.1 ETH
+    // callbackGasLimit: "500000", // 500,000 gas
+    vrfCoordinatorV2: '0x6168499c0cFfCaCD319c818142124B7A15E857ab',
+  },
+};

5ae990a9497dcab5caac539b89f4645faf913c3c
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-06-10 02:31:43 +0600

    checkup unit testing

--- hardhat-scc-lottery/backend/contracts/Raffle.sol
+++ hardhat-scc-lottery/backend/contracts/Raffle.sol
@@ -182,4 +182,8 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
     function getRequestConfirmations() public pure returns (uint256) {
         return REQUEST_CONFIRMATIONS;
     }
+
+    function getInterval() public view returns (uint256) {
+        return i_interval;
+    }
 }

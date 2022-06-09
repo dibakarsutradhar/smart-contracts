5178add0921b17a340d39a4f599d45b06a90660c
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-06-10 00:13:37 +0600

    deploy mocks and raffle script

--- hardhat-scc-lottery/backend/deploy/01-deploy-raffle.js
+++ hardhat-scc-lottery/backend/deploy/01-deploy-raffle.js
@@ -1,12 +1,52 @@
-const { network } = require('hardhat');
+const { network, ethers } = require('hardhat');
+const {
+  developmentChains,
+  networkConfig,
+} = require('../helper-hardhat-config');
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther('2');
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+  let vrfCoordinatorV2Address, subscriptionId;
+
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContract(
+      'VRFCoordinatorV2Mock'
+    );
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+    const transactionReceipt = await transactionResponse.wait(1);
+    subscriptionId = transactionReceipt.events[0].args.subId;
+    // Fund the subscription \
+    await vrfCoordinatorV2Mock.fundSubscription(
+      subscriptionId,
+      VRF_SUB_FUND_AMOUNT
+    );
+  } else {
+    vrfCoordinatorV2Address = networkConfig[chainId]['vrfCoordinatorV2'];
+    subscriptionId = networkConfig[chainId]['subscriptionId'];
+  }
+
+  const entranceFee = networkConfig[chainId]['raffleEntranceFee'];
+  const gasLane = networkConfig[chainId]['gasLane'];
+  const callbackGasLimit = networkConfig[chainId]['callbackGasLimit'];
+  const keepersUpdateInterval = networkConfig[chainId]['keepersUpdateInterval'];
+
+  const args = [
+    vrfCoordinatorV2Address,
+    entranceFee,
+    gasLane,
+    subscriptionId,
+    callbackGasLimit,
+    keepersUpdateInterval,
+  ];
 
   const raffle = await deploy('Raffle', {
     from: deployer,
-    args: [],
+    args: args,
     log: true,
     waitConfirmations: network.config.blockConfirmations || 1,
   });

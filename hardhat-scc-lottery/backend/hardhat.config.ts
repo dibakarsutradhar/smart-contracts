da52680816f18c431ecc4111f87b9984ef817e3a
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-06-12 22:53:41 +0600

    typescript conversation of hardhat config & helper

---
+++ hardhat-scc-lottery/backend/hardhat.config.ts
@@ -0,0 +1,57 @@
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-etherscan';
+import 'hardhat-deploy';
+import 'solidity-coverage';
+import 'hardhat-gas-reporter';
+import 'hardhat-contract-sizer';
+import 'dotenv/config';
+import { HardhatUserConfig } from 'hardhat/types';
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || 'https://eth-rinkeby';
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY || '0xkey';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key';
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 'key';
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      // blockConfirmations: 1,
+      // gasPrice: 130000000000,
+    },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+      chainId: 4,
+      // blockConfirmations: 6,
+    },
+  },
+  solidity: '0.8.7',
+  gasReporter: {
+    enabled: false,
+    outputFile: 'gas-report.txt',
+    noColors: true,
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: 'ETH',
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  mocha: {
+    timeout: 200000, // 400 seconds
+  },
+};
---
+++ hardhat-scc-lottery/backend/helper-hardhat-config.ts
@@ -0,0 +1,37 @@
+export interface networkConfigItem {
+  name?: string;
+  subscriptionId?: string;
+  gasLane?: string;
+  keepersUpdateInterval?: string;
+  raffleEntranceFee?: string;
+  callbackGasLimit?: string;
+  vrfCoordinatorV2?: string;
+}
+
+export interface networkConfigInfo {
+  [key: number]: networkConfigItem;
+}
+
+export const networkConfig: networkConfigInfo = {
+  4: {
+    name: 'rinkeby',
+    subscriptionId: '6258',
+    gasLane:
+      '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc', // 30 gwei
+    keepersUpdateInterval: '30',
+    raffleEntranceFee: '100000000000000000', //'100000000000000000', // 0.1 ETH
+    callbackGasLimit: '500000', // 500,000 gas
+    vrfCoordinatorV2: '0x6168499c0cFfCaCD319c818142124B7A15E857ab',
+  },
+  31337: {
+    name: 'hardhat',
+    raffleEntranceFee: '100000000000000000',
+    gasLane:
+      '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc', // 30 gwei
+    callbackGasLimit: '500000', // 500,000 gas
+    keepersUpdateInterval: '30',
+  },
+};
+
+export const developmentChains = ['hardhat', 'localhost'];
+export const VERIFICATION_BLOCK_CONFIRMATIONS = 6;

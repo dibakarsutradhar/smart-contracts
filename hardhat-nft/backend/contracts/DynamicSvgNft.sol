3d48c1182c3680f0b680969e36039896b48caf7b
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-06-25 23:42:10 +0600

    scc updated

--- hardhat-nft/backend/contracts/DynamicSvgNft.sol
+++ hardhat-nft/backend/contracts/DynamicSvgNft.sol
@@ -84,4 +84,20 @@ contract DynamicSvgNft is ERC721 {
                 )
             );
     }
+
+    function getLowSVG() public view returns (string memory) {
+        return i_lowImageURI;
+    }
+
+    function getHighSVG() public view returns (string memory) {
+        return i_highImageURI;
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return i_priceFeed;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
 }

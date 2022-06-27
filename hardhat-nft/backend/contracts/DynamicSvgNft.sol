61021c4bdf4436bdd4b30a5f1c5740bab9544d04
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-06-27 14:22:13 +0600

    debugged dynamic svg nft

--- hardhat-nft/backend/contracts/DynamicSvgNft.sol
+++ hardhat-nft/backend/contracts/DynamicSvgNft.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "base64-sol/base64.sol";
 
-contract DynamicSvgNft is ERC721 {
+contract DynamicSvgNft is ERC721, Ownable {
     uint256 private s_tokenCounter;
     string private i_lowImageURI;
     string private i_highImageURI;
-    string private constant base64EncodedSvgPrefix =
-        "data:image/svg+xml;base64,";
-    AggregatorV3Interface internal immutable i_priceFeed;
+
     mapping(uint256 => int256) public s_tokenIdToHighValue;
+    AggregatorV3Interface internal immutable i_priceFeed;
 
     event CreateNFT(uint256 indexed tokenId, int256 highValue);
 
@@ -32,18 +32,18 @@ contract DynamicSvgNft is ERC721 {
         pure
         returns (string memory)
     {
+        string memory baseURL = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode(
             bytes(string(abi.encodePacked(svg)))
         );
-        return
-            string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
     }
 
     function mintNft(int256 highValue) public {
         s_tokenIdToHighValue[s_tokenCounter] = highValue;
-        s_tokenCounter = s_tokenCounter + 1;
-        _safeMint(msg.sender, s_tokenCounter);
         emit CreateNFT(s_tokenCounter, highValue);
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
     }
 
     function _baseURI() internal pure override returns (string memory) {
@@ -53,10 +53,14 @@ contract DynamicSvgNft is ERC721 {
     function tokenURI(uint256 tokenId)
         public
         view
+        virtual
         override
         returns (string memory)
     {
-        require(_exists(tokenId), "URI Query for nonexistent token");
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI Query for nonexistent token"
+        );
         // string memory imageURI = "hi";
 
         (, int256 price, , , ) = i_priceFeed.latestRoundData();

e73e1c369a31146ba84b2c5a8d44a300890ddbc6
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-06-28 23:55:59 +0600

    hardhat boilerplate

---
+++ fullstack-nft-marketplace/backend-hardhat/hardhat.config.js
@@ -0,0 +1,59 @@
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('@nomiclabs/hardhat-ethers');
+require('hardhat-deploy');
+require('hardhat-gas-reporter');
+require('hardhat-contract-sizer');
+require('dotenv/config');
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || 'https://eth-rinkeby';
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY || '0xkey';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key';
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 'key';
+
+module.exports = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+      chainId: 4,
+      blockConfirmations: 6,
+    },
+  },
+  solidity: {
+    compilers: [
+      { version: '0.8.7' },
+      { version: '0.8.8' },
+      // { version: '0.6.12' },
+      // { version: '0.6.0' },
+    ],
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: 'gas-report.txt',
+    noColors: true,
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: 'ETH',
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+  },
+  mocha: {
+    timeout: 400000, // 400 seconds
+  },
+};

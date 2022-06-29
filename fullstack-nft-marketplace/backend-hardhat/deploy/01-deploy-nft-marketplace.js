e56bee6c7d49f576492804f1945b1e902e54b0ad
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-06-30 00:13:11 +0600

    nft marketplace deploy script

---
+++ fullstack-nft-marketplace/backend-hardhat/deploy/01-deploy-nft-marketplace.js
@@ -0,0 +1,30 @@
+const { network } = require('hardhat');
+const { developmentChains } = require('../helper-hardhat-config');
+const { verify } = require('../utils/verify');
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  let args = [];
+
+  const nftMarketPlace = await deploy('NftMarketplace', {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  });
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log('-----------------------------------------');
+    log('Verifying....!');
+    await verify(nftMarketPlace.address, args);
+  }
+
+  log('-----------------------------------------');
+};
+
+module.exports.tags = ['all', 'nftmarketplace'];

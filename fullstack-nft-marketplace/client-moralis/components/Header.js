4e4c17e755bea472f8b0abdd870789c21a2a704f
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-07-03 23:03:39 +0600

    homepage/nav

--- fullstack-nft-marketplace/client-moralis/components/Header.js
+++ fullstack-nft-marketplace/client-moralis/components/Header.js
@@ -5,14 +5,14 @@ const Header = () => {
   return (
     <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
       <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
-      <div>
+      <div className="flex flex-row items-center">
         <Link href="/">
-          <a>NFT Marketplace</a>
+          <a className="mr-4 p-6">Home</a>
         </Link>
         <Link href="/sell-nft">
-          <a>Sell NFT</a>
+          <a className="mr-4 p-6">Sell NFT</a>
         </Link>
-        <ConnectButton />;
+        <ConnectButton moralisAuth={false} />;
       </div>
     </nav>
   );

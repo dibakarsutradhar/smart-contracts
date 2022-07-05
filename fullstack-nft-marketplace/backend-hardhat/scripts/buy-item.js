50cafd04004e301200e558b240e0fd9f3e2c6680
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-07-06 02:26:59 +0600

    bought nft script && moralis cloud func update

---
+++ fullstack-nft-marketplace/backend-hardhat/scripts/buy-item.js
@@ -0,0 +1,27 @@
+const { ethers, network } = require('hardhat');
+const { moveBlocks } = require('../utils/move-blocks');
+
+const TOKEN_ID = 4;
+
+const buyItem = async () => {
+  const nftMarketplace = await ethers.getContract('NftMarketplace');
+  const basicNft = await ethers.getContract('BasicNFT');
+  const listing = await nftMarketplace.getListing(basicNft.address, TOKEN_ID);
+  const price = listing.price.toString();
+  const tx = await nftMarketplace.buyItem(basicNft.address, TOKEN_ID, {
+    value: price,
+  });
+  await tx.wait(1);
+  console.log('NFT Bought');
+
+  if (network.config.chainId == 31337) {
+    await moveBlocks(2, (sleepAmount = 1000));
+  }
+};
+
+buyItem()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });

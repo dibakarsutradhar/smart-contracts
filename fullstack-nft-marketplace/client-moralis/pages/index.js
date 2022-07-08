c41cfaf318485037a123b4bf7fd730be1caa6ae7
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-07-08 23:55:09 +0600

    nft listing using moralisQuery

--- fullstack-nft-marketplace/client-moralis/pages/index.js
+++ fullstack-nft-marketplace/client-moralis/pages/index.js
@@ -1,5 +1,30 @@
 import styles from '../styles/Home.module.css';
+import { useMoralisQuery } from 'react-moralis';
 
 export default function Home() {
-  return <div className={styles.container}>HI</div>;
+  const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
+    'ActiveItem',
+    (query) => query.limit(10).descending('tokenId')
+  );
+  console.log(listedNfts);
+
+  return (
+    <div className={styles.container}>
+      {fetchingListedNfts ? (
+        <div>Loading...</div>
+      ) : (
+        listedNfts.map((nft) => {
+          console.log(nft.attributes);
+          const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+            nft.attributes;
+          return (
+            <div>
+              Price: {price}. NftAddress: {nftAddress}. TokenId: {tokenId}.
+              Seller: {seller}.
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
 }
